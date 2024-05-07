5fcb31e8bcb08ec4f26dab864c58fe91b3322dd2
FrankRojas31 <fco3190@gmail.com>
2024-05-07 11:00:02 -0500

    Se agrega el CRUD Completo con ASP.NET con Entity Framework Core 3.0

---
+++ sistemaweb/Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace sistemaweb.Models
+{
+    public partial class Producto
+    {
+        public int Idproducto { get; set; }
+        public int Idcategoria { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Stock { get; set; }
+        public string Descripcion { get; set; }
+        public bool? Estado { get; set; }
+
+        public virtual Categoria IdcategoriaNavigation { get; set; }
+    }
+}
---
+++ sistemaweb/Models/dbproductosContext.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace sistemaweb.Models
+{
+    public partial class dbproductosContext : DbContext
+    {
+        public dbproductosContext()
+        {
+        }
+
+        public dbproductosContext(DbContextOptions<dbproductosContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categoria { get; set; }
+        public virtual DbSet<Producto> Producto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=Emma;Database=dbproductos;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.Idcategoria)
+                    .HasName("PK__categori__140587C766C8A80E");
+
+                entity.ToTable("categoria");
+
+                entity.HasIndex(e => e.Nombre)
+                    .HasName("UQ__categori__72AFBCC6EABA94E7")
+                    .IsUnique();
+
+                entity.Property(e => e.Idcategoria).HasColumnName("idcategoria");
+
+                entity.Property(e => e.Descripcion)
+                    .HasColumnName("descripcion")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Estado)
+                    .HasColumnName("estado")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.Idproducto)
+                    .HasName("PK__producto__DC53BE3CFC25FA14");
+
+                entity.ToTable("producto");
+
+                entity.HasIndex(e => e.Nombre)
+                    .HasName("UQ__producto__72AFBCC6108BF2D3")
+                    .IsUnique();
+
+                entity.Property(e => e.Idproducto).HasColumnName("idproducto");
+
+                entity.Property(e => e.Codigo)
+                    .HasColumnName("codigo")
+                    .HasMaxLength(64)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Descripcion)
+                    .HasColumnName("descripcion")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Estado)
+                    .HasColumnName("estado")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.Idcategoria).HasColumnName("idcategoria");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnName("nombre")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrecioVenta)
+                    .HasColumnName("precio_venta")
+                    .HasColumnType("decimal(11, 2)");
+
+                entity.Property(e => e.Stock).HasColumnName("stock");
+
+                entity.HasOne(d => d.IdcategoriaNavigation)
+                    .WithMany(p => p.Producto)
+                    .HasForeignKey(d => d.Idcategoria)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__producto__idcate__412EB0B6");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
